e6959b30c8031dcb65f1e1be81d66662954e88aa
alropidi93 <alvaro.picho@pucp.pe>
2018-07-18 17:25:57 -0500

    ANIMATIONNNNN

--- pak-ios/views/authentication/SplashController.swift
+++ pak-ios/views/authentication/SplashController.swift
@@ -20,43 +20,70 @@ import Firebase
 class SplashController: UIViewController {
     // Segues
     private let splash_identifier = "segue_splash_main"
+    @IBOutlet var iv_letter_logo: UIImageView!
     
     // Visual variables
     @IBOutlet weak var iv_logo: UIImageView!
     
     // Local variables
-    private var animation_parts: [UIImage] = [ UIImage(named: "dwb-pak-splash-2")!, UIImage(named: "dwb-pak-splash-3")!, UIImage(named: "dwb-pak-splash-4")!, UIImage(named: "dwb-pak-splash-5")!,UIImage(named: "dwb-pak-logo-name")!]
+    private var animation_parts: [UIImage] = [ UIImage(named: "dwb-pak-splash-1")!,UIImage(named: "dwb-pak-splash-2")!, UIImage(named: "dwb-pak-splash-3")!, UIImage(named: "dwb-pak-splash-4")!, UIImage(named: "dwb-pak-splash-5")!]
     
     // Common functions
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.getGUID()
+        print(self.iv_logo.frame.origin.y)
+        UIView.animate(withDuration: 0.25, animations: {
+            self.iv_logo.frame.origin.y -= 127
+        }){_ in
+            UIView.animateKeyframes(withDuration: 0.25, delay: 0.25, options: [], animations: {
+                self.iv_logo.frame.origin.y -= 0
+            },completion: {(finished: Bool) in
+                
+                self.iv_logo.image = self.animation_parts.last
+                self.iv_logo.animationImages = self.animation_parts
+                self.iv_logo.animationRepeatCount = 1
+                self.iv_logo.animationDuration = 1.0
+                self.iv_logo.startAnimating()
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    OperationQueue.main.addOperation {
+                        [weak self] in
+                        while (self?.iv_logo.isAnimating)! {
+                        }
+                        UIView.animate(withDuration: 1, animations: {
+                            self?.iv_letter_logo.isHidden = false
+                            self?.iv_logo.frame.origin.x -= 0
+                            self?.iv_letter_logo.frame.origin.x -= 0
+                        }){_ in
+                            UIView.animateKeyframes(withDuration: 1, delay: 0.25, options: [], animations: {
+                                self?.iv_letter_logo.frame.origin.x -= 20
+                                self?.iv_logo.frame.origin.x -= 20
+                                
+                            },completion: {(finished: Bool) in
+                                self?.performSegue(withIdentifier: (self?.splash_identifier)!, sender: self)
+                            })
+                        }
+                    }
+                }
+            })
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.iv_logo.animationImages = self.animation_parts
-        self.iv_logo.animationRepeatCount = 1
-        self.iv_logo.animationDuration = 1
-        self.iv_logo.startAnimating()
         
-        
-        
-        // must be careful that the animation duration and this stuff remains equal
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            OperationQueue.main.addOperation {
-                [weak self] in
-                while (self?.iv_logo.isAnimating)! {} //we wait until is finished
-                self?.performSegue(withIdentifier: (self?.splash_identifier)!, sender: self)
-            }
-        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func animate (imageView : UIImageView , images : [UIImage]){
+        
+    }
     func loginUser() {
        
         
